0d45ade283844ae07a30f98fbfc3459c118b2d08
zoe <sz491@georgetown.edu>
2024-03-12 01:31:21 -0400

    8-friendships-model-api-and-tests

---
+++ friendships/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Friendship(models.Model):
+    from_user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='following_friendship_set',
+    )
+    to_user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='follower_friendship_set',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        index_together = (
+            # 获取 我关注的所有人 (我的following们)，按照关注时间排序
+            ('from_user_id', 'created_at'),
+            # 获得 关注我的所有人(我的followers 因为他们都是to 我) 按照关注时间排序
+            ('to_user_id', 'created_at'),
+        )
+        unique_together = (('from_user_id', 'to_user_id'),)
+
+    def __str__(self):
+        return '{} followed {}'.format(self.from_user_id, self.to_user_id)
