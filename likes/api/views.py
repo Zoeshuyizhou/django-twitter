da4451df4481fec089a9b93354e9b04711d2cd71
zoe <sz491@georgetown.edu>
2024-03-17 12:07:02 -0400

    like create and cancel

---
+++ likes/api/views.py
@@ -0,0 +1,50 @@
+from likes.api.serializers import (
+    LikeSerializer,
+    LikeSerializerForCreate,
+    LikeSerializerForCancel,
+)
+from likes.models import Like
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from utils.decorators import required_params
+from rest_framework.decorators import action
+
+
+class LikeViewSet(viewsets.GenericViewSet):
+    queryset = Like.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = LikeSerializerForCreate
+
+    #用的post方法，所以request_attr 从data里找 如果没有tweet/comment+id 就会报错
+    @required_params(request_attr='data', params=['content_type', 'object_id'])
+    def create(self, request, *args, **kwargs):
+        serializer = LikeSerializerForCreate(
+            data=request.data,
+            context={'request': request},
+        )
+        if not serializer.is_valid():
+            return Response({
+                'message': 'Please check input',
+                'errors': serializer.errors,
+            }, status=status.HTTP_400_BAD_REQUEST)
+        instance = serializer.save()
+        return Response(
+            LikeSerializer(instance).data,
+            status=status.HTTP_201_CREATED,
+        )
+
+    @action(methods=['POST'], detail=False)
+    @required_params(request_attr='data', params=['content_type', 'object_id'])
+    def cancel(self, request, *args, **kwargs):
+        serializer = LikeSerializerForCancel(
+            data=request.data,
+            context={'request': request},
+        )
+        if not serializer.is_valid():
+            return Response({
+                'message': 'Please check input',
+                'errors': serializer.errors,
+            }, status=status.HTTP_400_BAD_REQUEST)
+        serializer.cancel()
+        return Response({'success': True}, status=status.HTTP_200_OK)
