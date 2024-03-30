6426067a4856ea347be9da42b0a6b494fc5c180c
zoe <sz491@georgetown.edu>
2024-03-30 15:54:17 -0400

    28-limit-list-size-in-redis

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -1,16 +1,15 @@
+from newsfeeds.services import NewsFeedService
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
+from tweets.services import TweetService
 from tweets.api.serializers import (TweetSerializer,
                                     TweetSerializerForCreate,
                                     TweetSerializerForDetail,
                                     )
 from tweets.models import Tweet
-from tweets.services import TweetService
-from newsfeeds.services import NewsFeedService
 from utils.decorators import required_params
 from utils.paginations import EndlessPagination
-
 
 class TweetViewSet(viewsets.GenericViewSet):
     """
@@ -46,16 +45,27 @@ class TweetViewSet(viewsets.GenericViewSet):
         ).order_by('-created_at')
         '''
         user_id = request.query_params['user_id']
-        tweets = TweetService.get_cached_tweets(user_id=user_id)
-
-        tweets = self.paginate_queryset(tweets)
+        cached_tweets = TweetService.get_cached_tweets(user_id)
+        page = self.paginator.paginate_cached_list(cached_tweets, request)
+        if page is None:
+            # 这句查询会被翻译为
+            # select * from twitter_tweets
+            # where user_id = xxx
+            # order by created_at desc
+            # 这句 SQL 查询会用到 user 和 created_at 的联合索引
+            # 单纯的 user 索引是不够的
+            queryset = Tweet.objects.filter(user_id=user_id).order_by('-created_at')
+            page = self.paginate_queryset(queryset)
         serializer = TweetSerializer(
-            tweets,
+            page,
             context={'request': request},
-            many=True)
+            many=True,
+        )
+        return self.get_paginated_response(serializer.data)
+
         # 一般来说 json 格式的 response 默认都要用 hash 的格式
         # 而不能用 list 的格式（约定俗成）
-        return self.get_paginated_response(data=serializer.data)
+
 
     def create(self, request, *args, **kwargs):
         """
